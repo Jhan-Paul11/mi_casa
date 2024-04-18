2b8c16f2e8d481e8e623bd6f6d71fc60386de4ea
Jhan-Paul11 <jhanpaul.bejarano@genstudents.org>
2024-04-18 14:51:21 -0500

    New change js, and css

--- mi_casa/CasaJhanPaul/casa/cuartos/Hola.js
+++ mi_casa/CasaJhanPaul/casa/cuartos/Hola.js
@@ -1,6 +1,15 @@
-function mi-nueva (var1,var2){
+/*function mi-nueva (var1,var2){
 for (i=0;i<Array.length;i++){
  let contador=suma var1+var2
 return contador
 }
+}*/
+
+let edad =Number(prompt ("digite su edad"))
+if (edad<=18){
+  if (edad<20){
+    console.log ("Tiene mas de 20 años");
+  }
+  console.log("Es mayor de edad");
 }
+console.log ("no es mayor de edad");
